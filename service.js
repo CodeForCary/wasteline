99d9adc8fac1e6331b8a8e2525c899379a529630
Ian Cillay <ian@cillay.com>
2018-11-11 16:22:37 -0500

    Added Veterans Day with weekend shift to Monday

--- service.js
+++ service.js
@@ -48,6 +48,12 @@
                     date = date.isoWeekday(-6)
                 }
                 return date;
+            })(year),            
+        "Veterans Day": (function (year) {
+                var eleventh = new Date(year, 10, 11),
+                    dayOfWeek = eleventh.getDay(),
+                    observed = dayOfWeek === 0 ? 12  : dayOfWeek === 6 ? 13 : 11;
+                return moment(new Date(year, 10, observed));
             })(year),
         "Veterans Day": (function (year) {
                 var eleventh = new Date(year, 10, 11),
